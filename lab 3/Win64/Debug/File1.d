f0a8904e812f561b28d7ce6e41a483d3e6443ee9
Янушонок Даниил <79411351+Yadenian@users.noreply.github.com>
2021-05-18 12:28:54 +0300

    Add files via upload

---
+++ lab 3/Win64/Debug/File1.d
@@ -0,0 +1,16 @@
+.\Win64\Debug\File1.o: File1.c \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\stdio.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_stddef.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_defs.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_null.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_nfile.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\stdarg.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\tchar.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_str.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\locale.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_loc.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\mem.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\time.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\stdlib.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\search.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\errno.h
---
+++ lab1/Win64/Debug/File1.d
@@ -0,0 +1,12 @@
+.\Win64\Debug\File1.o: File1.c \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\stdio.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_stddef.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_defs.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_null.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_nfile.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\stdarg.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\tchar.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_str.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\locale.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\_loc.h \
+  c:\program\ files\ (x86)\embarcadero\studio\20.0\include\windows\crtl\mem.h
